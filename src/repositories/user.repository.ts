aa0c13ee2a919cee444d2af3705639bb94dfd47f
Jonathan Faria <jonathanfdsn@gmail.com>
2022-08-01 18:28:49 -0300

    Comunicação das rotas com o banco de dados

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -9,9 +9,61 @@ class UserRepository {
            FROM application_user; 
         `;
         
-        const result = await database.query<User>(query);
-        const rows = result.rows;
+        const { rows } = await database.query<User>(query);
         return rows || [];
+    }
+
+    async findById(uuid: string): Promise<User> {
+        const query = `
+           SELECT uuid, username 
+           FROM application_user
+           WHERE uuid = $1; 
+        `;
+
+        const values = [uuid];
+        
+        const { rows } = await database.query<User>(query, values);
+        
+        const [ user ] = rows;
+        
+        return user;
+    }
+
+    async addByUser(user: User): Promise<string> {
+        const query = `
+           INSERT INTO application_user (username, password)
+           VALUES ($1, crypt($2, 'el_salty'))
+           RETURNING uuid; 
+        `;
+
+        const values = [user.username, user.password];
+
+        const { rows } = await database.query<{uuid: string}>(query, values);
+        const [ newUser ] = rows;
+
+        return newUser.uuid;
+    }
+
+    async updateById(user: User): Promise<void> {
+        const query = `
+           UPDATE application_user
+           SET username = $1, password = crypt($2, 'el_salty')
+           WHERE uuid = $3; 
+        `;
+
+        const values = [user.username, user.password, user.uuid];
+
+        await database.query(query, values);
+    }
+
+    async removeUser (uuid: string): Promise<void> {
+        const query = `
+           DELETE FROM application_user 
+           WHERE uuid = $1; 
+        `;
+
+        const values = [uuid];
+        await database.query(query, values);
     }
 }
 
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -11,28 +11,34 @@ usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction)
 });
 
 //get para users uuid
-usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+usersRoute.get('/users/:uuid', async (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(200).send({uuid}); 
+    const user = await userRepository.findById(uuid);
+    res.status(200).send(user); 
 });
 
 //post para users
-usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction) => {
     const newUser = req.body;
-    res.status(201).send(newUser);
+    const newId = await userRepository.addByUser(newUser);
+    res.status(201).send(newId);
 });
 
 //put para users
-usersRoute.put('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+usersRoute.put('/users/:uuid', async (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
     const modifiedUser = req.body;
-
     modifiedUser.uuid = uuid;
-    res.status(200).send({modifiedUser});
+
+    await userRepository.updateById(modifiedUser);
+
+    res.status(200).send();
 });
 
 //delete para users
-usersRoute.delete('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+usersRoute.delete('/users/:uuid', async (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    await userRepository.removeUser(uuid);
     res.sendStatus(200);
 });
 
