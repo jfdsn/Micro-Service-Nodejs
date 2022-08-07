e0ed11b22960fac85132093df2535d0f236b72ba
Jonathan Faria <93546362+jfdsn@users.noreply.github.com>
2022-08-07 12:43:30 -0300

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 //Arquivo de conexão com o banco de dados.
 
-const connectionString = 'postgres://wekkayqm:hEL_jbtYQ6IRrS6NfYJE71gZ_XBC3K2Y@kesavan.db.elephantsql.com/wekkayqm';
+const connectionString = '';
 
 const database = new Pool({connectionString});
 
